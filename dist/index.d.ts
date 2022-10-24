ff31cbe29c5653c3be981d5a003ddf9601efc017
caique.moraes <caique.moraes@compasso.com.br>
2022-10-24 17:43:38 -0300

    generate esm and cjs files

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,11 @@
+/**
+ * Check if both data structures are equals
+ * @date 04/10/2022 - 21:10:18
+ *
+ * @param {*} objA
+ * @param {*} objB
+ * @returns {boolean} true = equal, false = not equal
+ */
 declare const deepStrictEqual: (objA: any, objB: any) => boolean;
 
 export { deepStrictEqual };
